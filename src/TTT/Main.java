bc697c767c8658e8375bf997512475c0c954159d
Kayla <kaylaputrimaharani@gmail.com>
2024-12-17 13:00:12 +0700

    Game Base

---
+++ src/TTT/Main.java
@@ -0,0 +1,13 @@
+package TTT;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Connect Four Game");
+        TTT game = new TTT();
+        game.play();
+    }
+}
